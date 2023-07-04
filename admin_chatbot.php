d65cbad8d37ed9fa160b3d19091052e092fe6280
Amirul Momenin <patainc@gmail.com>
2023-07-04 23:49:09 +0600

    Update admin_chatbot.php

--- admin_chatbot.php
+++ admin_chatbot.php
@@ -7,3 +7,7 @@ But You can set your short code in any page content or set in the footer to get 
 
 
 Add you data <a href="admin.php?page=chatdata">Chat data</a>
+
+
+<br><br>
+ <a href="https://www.paypal.com/paypalme/patacorporation">Donate</a>
